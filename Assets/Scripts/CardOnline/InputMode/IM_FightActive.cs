47d8cf9a3e2bd8bba9b357a6b6c159435dae0d38
BruiseBuill <BruiseBuill@163.com>
2025-12-09 23:07:24 +0800

    12-9

--- Assets/Scripts/CardOnline/InputMode/IM_FightActive.cs
+++ Assets/Scripts/CardOnline/InputMode/IM_FightActive.cs
@@ -1,4 +1,5 @@
 using BF;
+using CardOnline.Card;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,14 @@ namespace CardOnline
         [SerializeField] GenericEventChannel<Vector3,Vector3> ch_OnNormalDrag;
 
         [SerializeField] GenericEventChannel<Vector3> ch_Accelerate;
+
+
+
+
+        //
+        [SerializeField] GenericEventChannel<> ch_onScanCard;
+        [SerializeField]
+        [SerializeField] LayerMask cardLayer;
 
         public override void SetActMode()
         {
@@ -29,7 +38,30 @@ namespace CardOnline
             InputManager.onDrag = delegate { };
             ch_Accelerate.RemoveListener(Accelerate);
         }
+        void OnPointDown(Vector3 screenPos)
+        {
+            var card = RaycastCard(screenPos);
+            if (card != null)
+            {
 
+            }
+
+        }
+        MagicCard RaycastCard(Vector3 screenPos)
+        {
+            Ray ray = new Ray(Camera.main.ScreenToWorldPoint(screenPos), Vector3.forward);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, 1 << cardLayer)) 
+            {
+                Collider collider = hitInfo.collider;
+                MagicCard card = collider.GetComponentInParent<MagicCard>();
+                return card;
+            }
+            else
+            {
+                return null;
+            }
+        }
         public override void Update()
         {
             
---
+++ Assets/Scripts/CardOnline/InputMode/IM_FightSilence.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardOnline
+{
+ 	public class IM_FightSilence : MonoBehaviour
+	{
+    	
+	}
+}
